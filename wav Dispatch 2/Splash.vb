654b97f86f653d40a447067e30f4214846e5990c
Will Jones <willjones97@hotmail.com>
2019-07-03 13:04:23 +0100

    kjinjerrwk'rwk

--- wav Dispatch 2/Splash.vb
+++ wav Dispatch 2/Splash.vb
@@ -1,12 +1,4 @@
 ﻿Public Class Splash
-    Private Sub Button1cli_Click(sender As Object, e As EventArgs)
-        Timer1.Start()
-        Enabled = False
-
-
-
-    End Sub
-
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         ProgressBar1.Increment(1)
         If ProgressBar1.Value = 100 Then
@@ -17,9 +9,11 @@
         Label1.Text = ProgressBar1.Value & (" %")
     End Sub
 
-
-
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
 
     End Sub
 End Class
