5818491e9bc2995116bff03bce2b4f7964ec70ba
Will Jones <willjones97@hotmail.com>
2019-07-03 10:28:58 +0100

    wqeqwfdp;te2

--- wav Dispatch 2/Form2.Designer.vb
+++ wav Dispatch 2/Form2.Designer.vb
@@ -30,7 +30,7 @@ Partial Class Form2
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(394, 9)
+        Me.Label1.Location = New System.Drawing.Point(399, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(110, 31)
         Me.Label1.TabIndex = 0
