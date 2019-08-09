907c3a35be3e6ecbe9e70350540170fe10ce4b86
Will Jones <willjones97@hotmail.com>
2019-08-09 15:12:09 +0100

    ngfgre5yhvfd

--- wav Dispatch 2/Form3.Designer.vb
+++ wav Dispatch 2/Form3.Designer.vb
@@ -115,7 +115,7 @@ Partial Class Form3
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(753, 376)
+        Me.PictureBox1.Location = New System.Drawing.Point(763, 376)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(135, 29)
         Me.PictureBox1.TabIndex = 18
